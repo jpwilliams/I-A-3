c30e246d024ef07d4301c3cf78f9825fff920650
Jack Williams <jpwilliams@ahoyworld.co.uk>
2013-09-05 22:13:55 +0100

    Re-arm system updates; Split AW_fnc_baseVehSpawn into two functions, one SQF that calls the FSM to have returning the created object possible; Updated Emplacements FSM to account for the truck failing to be created; Added image of Chesty Puller as HQ; Added 12 "Objective Taken" radio sounds to be played at the end of each Main AO; Added CfgRadio .hpp configs

---
+++ sounds/radio/hq.hpp
@@ -0,0 +1,83 @@
+class ObjectiveTaken1
+{
+	name = "ObjectiveTaken1";
+	sound[] = {"\sounds\radio\ObjectiveTaken1.ogg", db-100, 1.0};
+	title = "Fan-fucking-tastic job, boys! Hell, at this rate, we'll just fucking take Greece!";
+};
+
+class ObjectiveTaken2
+{
+	name = "ObjectiveTaken2";
+	sound[] = {"\sounds\radio\ObjectiveTaken2.ogg", db-100, 1.0};
+	title = "Objective complete! Now get back to base a re-arm; we've got more targets for you boys to hit.";
+};
+
+class ObjectiveTaken3
+{
+	name = "ObjectiveTaken3";
+	sound[] = {"\sounds\radio\ObjectiveTaken3.ogg", db-100, 1.0};
+	title = "AO is neutralised. Good job.";
+};
+
+class ObjectiveTaken4
+{
+	name = "ObjectiveTaken4";
+	sound[] = {"\sounds\radio\ObjectiveTaken4.ogg", db-100, 1.0};
+	title = "That objective's taken. Let's get out of there and re-arm.";
+};
+
+class ObjectiveTaken5
+{
+	name = "ObjectiveTaken5";
+	sound[] = {"\sounds\radio\ObjectiveTaken5.ogg", db-100, 1.0};
+	title = "Main AO is neutralised. We'll get another target for you; stand by.";
+};
+
+class ObjectiveTaken6
+{
+	name = "ObjectiveTaken6";
+	sound[] = {"\sounds\radio\ObjectiveTaken6.ogg", db-100, 1.0};
+	title = "Stellar job, boys. We'll patch a new target through to you; stand by.";
+};
+
+class ObjectiveTaken7
+{
+	name = "ObjectiveTaken7";
+	sound[] = {"\sounds\radio\ObjectiveTaken7.ogg", db-100, 1.0};
+	title = "Objective taken! Now get the hell back to base.";
+};
+
+class ObjectiveTaken8
+{
+	name = "ObjectiveTaken8";
+	sound[] = {"\sounds\radio\ObjectiveTaken8.ogg", db-100, 1.0};
+	title = "Now that is what I like to see! Good job, boys. Objective taken.";
+};
+
+class ObjectiveTaken9
+{
+	name = "ObjectiveTaken9";
+	sound[] = {"\sounds\radio\ObjectiveTaken9.ogg", db-100, 1.0};
+	title = "Looks like all OPFOR forces have been neutralised in that Main AO, boys. Good job. Get back to base, re-arm and we'll get you another target.";
+};
+
+class ObjectiveTaken10
+{
+	name = "ObjectiveTaken10";
+	sound[] = {"\sounds\radio\ObjectiveTaken10.ogg", db-100, 1.0};
+	title = "Now that sure made me miss my days of being on the ground. Good job boys. Objective taken.";
+};
+
+class ObjectiveTaken11
+{
+	name = "ObjectiveTaken11";
+	sound[] = {"\sounds\radio\ObjectiveTaken11.ogg", db-100, 1.0};
+	title = "Objective taken, boys. Oo-rah!";
+};
+
+class ObjectiveTaken12
+{
+	name = "ObjectiveTaken12";
+	sound[] = {"\sounds\radio\ObjectiveTaken12.ogg", db-100, 1.0};
+	title = "That shit was like watching a fucking film! Amazing job! Main AO is taken. Get back to base and we'll get you another one.";
+};
